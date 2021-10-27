e52a093e38b039165c42ac6ac33ac6bff1574ac6
Maxime Moreillon <m.moreillon@gmail.com>
2021-10-27 01:09:53 +0000

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,84 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>File POSTer</h1>
+
+    <form @submit.prevent="post_file()">
+      <div class="">
+        <label>URL</label>
+        <input type="text" v-model="url">
+      </div>
+      <div class="">
+        <label>File</label>
+        <input type="file" ref="file_input">
+      </div>
+      <div class="">
+        <label>Field name</label>
+        <input type="text" v-model="field_name">
+      </div>
+      <div class="">
+        <button type="submit">POST</button>
+      </div>
+
+    </form>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
+  },
+  data(){
+    return {
+      url: 'http://192.168.1.2:7070',
+      field_name: 'image',
+      posting: false,
+    }
+  },
+  methods: {
+    post_file(){
+      this.posting = true
+      
+      const headers = {'Content-Type': 'multipart/form-data' }
+      const formData = new FormData()
+      formData.append(this.field_name, this.$refs.file_input.files[0])
+
+      this.axios.post(this.url, formData, { headers })
+        .then( () => {
+          alert('POST successful')
+        })
+        .catch( (error) => {
+
+          if(error.response) console.error(error.response.data)
+          else console.error(error)
+
+          alert(`POST failed`)
+        })
+        .finally( () => {
+          this.posting = false
+        })
+
+
+    }
   }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: sans-serif;
+}
+form > div {
+  margin: 1em 0;
+}
+
+form label {
+  margin-right: 1em;
 }
 </style>
